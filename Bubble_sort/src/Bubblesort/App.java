5c1f1a8cd27765201880b17f5373d0dad680c350
omkar <omkargad007@gmail.com>
2018-07-12 17:01:34 +0530

    bubble

---
+++ Bubble_sort/src/Bubblesort/App.java
@@ -0,0 +1,13 @@
+package Bubblesort;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int arr[]={4,2,1,3,5,9,8,7,6};
+		// Bubble bubble = new Bubble(arr);
+		 Bubble.sort(arr);
+		
+	}
+
+}
